81953a6d6e9140b6173fa7d39715722172f40d57
Augusto Alejandro Cognigni <acognigni@protonmail.com>
2024-11-13 20:36:00 -0300

    agregue un monton de paquetes opcionales

--- svelte.config.js
+++ svelte.config.js
@@ -1,3 +1,4 @@
+import { mdsvex } from 'mdsvex';
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
@@ -5,7 +6,7 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+	preprocess: [vitePreprocess(), mdsvex()],
 
 	kit: {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
@@ -14,7 +15,9 @@ const config = {
 		adapter: adapter({
 			fallback: 'index.html'
 		})
-	}
+	},
+
+	extensions: ['.svelte', '.svx']
 };
 
 export default config;
